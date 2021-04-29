64e0ed9bdba9f88418cc31d46780709ab2659470
GrandpaOfAll <christsit01@gmail.com>
2021-04-29 19:52:31 +0300

    Added Locations File

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Main {
@@ -6,8 +11,30 @@ public class Main {
 	
 	public static void main(String[] args) {	
 		ArrayList<Player> allPlayers=new ArrayList<Player>();
+		ArrayList<Location> locations;
+		
 		new LoginScreenGUI();
-	
+		
+		//new GUI();
+		File file = new File("Locations.ser");
+		
+		try {
+			FileInputStream fIn = new FileInputStream(file);
+			ObjectInputStream in = new ObjectInputStream(fIn);
+			
+			locations = (ArrayList<Location>) in.readObject();
+			in.close();
+			fIn.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
