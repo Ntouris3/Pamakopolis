d23778c6deb1f71e8f9eeeffadfc0c374670f92e
Anastasios Athanasiadis <tasos1615@gmail.com>
2021-05-09 20:10:19 +0300

    Add files via upload

--- src/Street.java
+++ src/Street.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class Street extends Property{
 	
-	private String colour;
+	public String colour;
 	private int propertiesInColour;
 	private int rent[];
 	public int houses;
 	public int hotel ;
-	private int houseCost;
-	private int hotelCost;
+	public int houseCost;
+	public int hotelCost;
 
 	
 	
@@ -28,6 +28,9 @@ public class Street extends Property{
 
 	public void Build(Player player, int number) {
 		//GUI input for number of houses #1-4 and hotel #5
+		//αν θελει να χτισει ενα hotel κατευθειαν, τι γινεται??
+		//hotelcost πρεπει να ειναι housecost*5
+		//θελει pop up window επιβεβαιωσης
 		if (number==5) {
 			player.balance=player.balance-hotelCost;
 			hotel++;
@@ -39,21 +42,24 @@ public class Street extends Property{
 	}
 	
 	public void Demolish(Player player, int number) {
-		//XARA DES NAOYME
 		if (number==5) {
 			hotel--;
+			player.balance=player.balance+(hotelCost/2);
 		}else {
 			houses=houses-number;
+			player.balance=player.balance+(houseCost/2)*number;
 		}
 	}
 
 
 	public int CalcRent(Player player) {
 		int sum=0;
-		sum=sum+rent[houses];
 		
-		if (hotel>=1) {
-			sum=sum+(rent[5]*hotel);
+		if (hotel==1) {
+			sum=sum+rent[5];
+		}
+		else if(hotel==0) {
+			sum=sum+rent[houses];			
 		}
 		
 		return sum;
