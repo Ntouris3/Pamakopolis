32b6428f0eb09aa4ac2cb4ee8b71c204da4db67c
TeoAth01 <82370290+TeoAth01@users.noreply.github.com>
2021-05-09 19:51:11 +0300

    Updated Player

--- src/Player.java
+++ src/Player.java
@@ -15,6 +15,7 @@ public class Player {
 		this.name = name;
 		this.piece = piece;
 		properties = new ArrayList<Property>();
+		Main.allPlayers.add(this);
 	}
 	
 	public void AddBalance (int amount) {
@@ -28,13 +29,25 @@ public class Player {
 	public void Buy (Property prop) {
 		properties.add(prop);
 		this.ReduceBalance(balance);
+		prop.owner = this;
 	}
 	
-	public void Sell (Property prop) {
-		properties.remove(prop);
-		this.ReduceBalance(balance);
-	}
-	public void ChangePosition (int newPosition) {
+
+
+
+	public void ChangePosition (int newPos) {
+		int temp = newPos % 40;
+		
+		this.piece.MoveOnBoard(this, newPos % 40);
+		if (temp == newPos) {         
+				this.position = newPos;
+			if (temp == 0) AddBalance(200);   //0 is the Start
+		}
+		else 
+		{
+			this.position = newPos % 40;
+			AddBalance(200);
+		}
 		
 	}
 	
@@ -43,11 +56,18 @@ public class Player {
 	}
 	
 	public void AddToMortgage(Property prop) {
-		
+	if (!prop.isMortgaged)
+		{
+			AddBalance(prop.mortgage);	
+			prop.isMortgaged = true;
+		}	
 	}
 	
 	public void Unmortgage (Property prop) {
-		
+		if (prop.isMortgaged) {
+		  ReduceBalance((int)1.1*(prop.mortgage));
+		  prop.isMortgaged = false;  
+		}
 	}
 	public ArrayList<Property> PropertiesToBuildIn (){
 		// svhste to otan einai na grapsete to kwdika , to egrapsa gia na mhn vgazei errros
@@ -56,11 +76,20 @@ public class Player {
 	}
 	
 	public boolean isBankrupt () {
-		// svhste to otan einai na grapsete to kwdika , to egrapsa gia na mhn vgazei errros
-		return false;
-	}
-	public static boolean playerExists (String Name) {
-		// svhste to otan einai na grapsete to kwdika , to egrapsa gia na mhn vgazei errros
+		int sum1 = 0, sum2 = 0;
+		
+		for (int i=0; i<properties.size(); i++)
+		{
+			sum1 += properties.get(i).price;//mortgage
+		}
+		
+		for (int i=0; i<streets.size();i++)
+		{
+			sum2 += (streets.get(i).hotelCost + streets.get(i).houseCost);
+		}
+		
+		if (balance<0 && sum1==0 && sum2==0)
+			return true;
 		return false;
 	}
 }
